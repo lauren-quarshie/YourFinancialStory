327dc6dea05e062c35804abeaf525fecedb42deb
lauren-quarshie <98128799+lauren-quarshie@users.noreply.github.com>
2025-09-10 15:10:18 +0100

    Delete src directory

--- src/DesignSpecsApp.tsx
+++
@@ -1,9 +0,0 @@
-import { DesignSpecs } from './components/DesignSpecs';
-
-export default function DesignSpecsApp() {
-  return (
-    <div className="min-h-screen bg-background">
-      <DesignSpecs />
-    </div>
-  );
-}
